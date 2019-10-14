329f17bb6700664488c50bba93cc938a04e6eb0e
rossmccrann <rossmccrann@192.168.0.122>
2019-10-14 21:27:34 +0100

    Binary Tree Updated

--- Lowest_Common_Ancestor/src/Binary_Tree.java
+++ Lowest_Common_Ancestor/src/Binary_Tree.java
@@ -36,4 +36,31 @@ public class Binary_Tree {
         // check if left subtree or right subtree is LCA 
         return (left_lca != null) ? left_lca : right_lca; 
     } 
+    
+
+	public static void main(String[] args) {
+		 Binary_Tree tree = new Binary_Tree(); 
+	        tree.root = new Node(1); 
+	        tree.root.left = new Node(2); 
+	        tree.root.right = new Node(3); 
+	        tree.root.left.left = new Node(4); 
+	        tree.root.left.right = new Node(5); 
+	        tree.root.right.left = new Node(6); 
+	        tree.root.right.right = new Node(7); 
+	  
+	        Node lca = tree.findLCA(4, 5); 
+	        
+	        if (lca != null) 
+	            System.out.println("LCA(4, 5) = " + lca.data); 
+	        else
+	            System.out.println("Keys are not present"); 
+	  
+	        lca = tree.findLCA(4, 10); 
+	        
+	        if (lca != null) 
+	            System.out.println("LCA(4, 10) = " + lca.data); 
+	        else
+	            System.out.println("Keys are not present"); 
+	}
+	
 }
